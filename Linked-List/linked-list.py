bed47b14082a3131463a63694d0e264edf49606d
Majid <mjghcr@gmail.com>
2021-09-30 16:28:24 +0330

    add pop, get and set, insert at the specified index, remove from list and reverse the list methods to master

--- Linked-List/linked-list.py
+++ Linked-List/linked-list.py
@@ -34,11 +34,10 @@ class LinkedList:
         if self.length == 0:
             return None
         elif self.length == 1:
-            element = self.head.value
+            element = self.head
             self.head = None
-            self.head.next = None
-            self.length == 0
-            return element
+            self.length = 0
+            return element.value
         
         pre = self.head
         temp = self.head
@@ -60,3 +59,82 @@ class LinkedList:
         self.head.next = temp
         self.length += 1
 
+
+    def pop_first(self): # Pop an element from the beginning
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            element = self.head
+            self.head = None
+            self.length = 0
+            return element
+
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+
+    def get_value(self, index): # Get with index
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    
+    def set_value(self, index, value): # Set specified value at the exact index
+        temp = self.get_value(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+
+    def insert(self, index, value): # Insert with the index
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get_value(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+
+    def remove(self, index): # Remove with the given index
+        if index < 0 or index >= self.length:
+            return None
+        if self.length == 0:
+            return self.pop_first(index)
+        elif index == self.length - 1:
+            return self.pop()
+        prev = self.get_value(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+
+    def reverse(self):
+        # Swapping head with tail
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        after = temp.next
+        before = None
+        # Iterate through the list to reverse the order
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+        
