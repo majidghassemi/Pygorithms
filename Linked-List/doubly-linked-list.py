33cadd7e621668b1029a7e8e58284fb33eef4f92
Majid <mjghcr@gmail.com>
2021-10-14 10:38:58 +0330

    get, set and insert methods for DLL add

--- Linked-List/doubly-linked-list.py
+++ Linked-List/doubly-linked-list.py
@@ -60,3 +60,46 @@ class DoublyLinkedList: # Create our DLL constructor and initalize it
             self.head = new_node
         self.length += 1
         return True
+    
+
+    def get(self, index): # Get specified value with index
+        if index < 0 or index >= self.length:
+            return None
+        if index < self.length / 2:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+
+    
+    def set_value(self, index, value): # Set specified value at the exact index
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+
+    def insert(self, index, value): # Insert value at the specified index
+        if index < 0 or index > self.length:
+            return False or None
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+        new_node.next = after
+        new_node.prev = before
+        before.next = new_node
+        after.prev = new_node
+        self.length += 1
+        return True
+        
+
+    
